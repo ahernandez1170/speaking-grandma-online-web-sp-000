24781ed1ee518f3f4028e99f838eb73e1a117826
Austin Hernandez <ahernandez1170@gmail.com>
2020-12-31 02:30:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,14 +13,14 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-
-    
+string = "I LOVE YOU GRANDMA!"    
     def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
-  else "I LOVE YOU TOO PUMPKIN!"
+  else string
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end 
 
